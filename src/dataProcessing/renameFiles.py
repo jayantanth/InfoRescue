23f35f13de94c049635e43ddc8002b36a21f79bc
Abhishek_Gupta <knoxxs@hotmail.com>
2013-06-11 00:05:27 +0530

    Added renameFiles.py to rename all the files in the corpus according to some pattern.
    Ocred and Original files have different prefix. So ran this file on
    original-text files so that Ocred and Original files have the same
    name.

---
+++ src/dataProcessing/renameFiles.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+
+if len(sys.argv) == 2 and sys.argv[1] == '-h':
+	print("Usage: ./renameFiles.py <%> <Data>")
+	sys.exit()
+elif len(sys.argv) != 2:
+	print "Invalid number of arguments. Try './makeTestData.py -h'"
+	sys.exit()
+
+corpus = sys.argv[1]
+
+pattern = re.compile("[a-zA-Z]+")
+
+for (dirpath, dirname, filenames) in os.walk(corpus):
+	if pattern.match(dirpath.split("/")[-1]):
+		for f in filenames:
+			os.rename(dirpath + "/" + f,dirpath + "/" + f[:-7] + "txt")
