cd4d5189186b0eb5df263263aff8721a06cd373a
Abhishek_Gupta <knoxxs@hotmail.com>
2013-06-26 19:18:57 +0530

    made lcs plcs

--- src/includes/longestCommonSubsequence.py
+++ src/includes/longestCommonSubsequence.py
@@ -1,40 +1,67 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import copy
+import json
+import codecs
 
-def printTable(table, n, m):
-    for i in range(n+1):
-        for j in range(m+1):
+THRESHOLD = 0.02
+modelDict = {}
+
+def getCEM(CEMFile):
+    global modelDict
+    with codecs.open(CEMFile, 'r', encoding='utf8') as output:
+        modelDict = json.load(output)
+    for char in modelDict:
+        for mappedChar in modelDict[char]:
+           modelDict[char][mappedChar] = float(modelDict[char][mappedChar])
+
+def prob_equals(ocrChar, origChar):
+    if modelDict[origChar][ocrChar] >= THRESHOLD:
+        return True
+    return False
+
+def printTable(table, m, n):
+    for i in range(m+1):
+        for j in range(n+1):
             print table[i,j],
         print
 
-def lcs(x, y):
-    n = len(x)
-    m = len(y)
+def removeDuplicate(lcss):
+    setLcss = []
+    for i in lcss:
+        if i not in setLcss:
+            setLcss.append(i)
+    return setLcss
+
+def lcs(ocr, orig):
+    ocr, orig = ocr.decode('utf8'), orig.decode('utf8')
+    m = len(ocr)
+    n = len(orig)
     table = dict()
 
-    for i in range(n+1):
-        for j in range(m+1):
+    for i in range(m+1):
+        for j in range(n+1):
             if i == 0 or j == 0:
                 table[i, j] = 0
-            elif x[i-1] == y[j-1]:
+            elif prob_equals(ocr[i-1], orig[j-1]):
                 table[i, j] = table[i-1, j-1] + 1
             else:
                 table[i, j] = max(table[i-1, j], table[i, j-1])
 
-    # printTable(table, n, m)
+    # printTable(table, m, n)
     lcss = [[]]
     def recon(i, j, index):
         if i == 0 or j == 0:
             return
-        elif x[i-1] == y[j-1]:
+        elif prob_equals(ocr[i-1], orig[j-1]):
             if table[i-1, j] > table[i-1, j-1]:
                 lcss.append(copy.deepcopy(lcss[index]))
                 recon(i-1, j, len(lcss) - 1)
             if table[i, j-1] > table[i-1, j-1]:
                 lcss.append(copy.deepcopy(lcss[index]))
                 recon(i, j-1, len(lcss) - 1)
-            # lcss[index].insert(0, (x[i-1],i-1, j-1))
+            # lcss[index].insert(0, (ocr[i-1],i-1, j-1))
             lcss[index].insert(0, (i-1, j-1))
             recon(i-1, j-1, index)
         elif table[i-1, j] == table[i, j-1]:
@@ -47,8 +74,10 @@ def lcs(x, y):
         else:
             recon(i, j-1, index)
 
-    recon(n, m, 0)
-    return lcss
+    recon(m, n, 0)
+    return removeDuplicate(lcss)
 
 # print lcs('ABCBDAB', 'BDCABA')
 # print lcs('AGCGA', 'CAGATAGAG')
+# getCEM('../features/characterErrorModelProbalistic.json')
+# print lcs('চাহুর','রাহুর')
