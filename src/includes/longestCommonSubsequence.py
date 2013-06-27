4fa703d141946411fd347e8a405eede9615d39f7
Abhishek_Gupta <knoxxs@hotmail.com>
2013-06-28 01:07:42 +0530

    Modify to return the probability with LCS, handle those mappings which don't exist in the CEM, added minProb to CEM to ignore 0 mutiplication problem i.e. applied smoothing

--- src/includes/longestCommonSubsequence.py
+++ src/includes/longestCommonSubsequence.py
@@ -5,8 +5,9 @@ import copy
 import json
 import codecs
 
-THRESHOLD = 0.02
+THRESHOLD = 0.4
 modelDict = {}
+minProb = 1
 
 def getCEM(CEMFile):
     global modelDict
@@ -16,15 +17,32 @@ def getCEM(CEMFile):
         for mappedChar in modelDict[char]:
            modelDict[char][mappedChar] = float(modelDict[char][mappedChar])
 
-def prob_equals(ocrChar, origChar):
-    if modelDict[origChar][ocrChar] >= THRESHOLD:
-        return True
-    return False
+def getMinimumProbability():
+    return min(min(mappedChars.values()) for mappedChars in modelDict.values())
 
-def printTable(table, m, n):
-    for i in range(m+1):
-        for j in range(n+1):
-            print table[i,j],
+def increaseProbability(minProb):
+    for origChar in modelDict:
+        for ocrChar in modelDict[origChar]:
+            modelDict[origChar][ocrChar] += minProb
+
+def prob_equals(ocrChar, origChar):
+    try:
+        if modelDict[origChar][ocrChar] >= THRESHOLD:
+            return True
+        return False
+    except KeyError:
+        return False
+
+def getProb(ocrChar, origChar):
+    try:
+        return modelDict[origChar][ocrChar]
+    except KeyError:
+        return minProb
+
+def printTable(table):
+    for row in table:
+        for column in row:
+            print "%3s"%column,
         print
 
 def removeDuplicate(lcss):
@@ -49,23 +67,28 @@ def lcs(ocr, orig):
             else:
                 table[i, j] = max(table[i-1, j], table[i, j-1])
 
-    # printTable(table, m, n)
-    lcss = [[]]
+    # printTable(table)
+    lcss = [[1,[]]]
+
+    def lcssAppend(index):
+        lcss.append(copy.deepcopy([lcss[index][0], lcss[index][1]]))
+
     def recon(i, j, index):
         if i == 0 or j == 0:
             return
         elif prob_equals(ocr[i-1], orig[j-1]):
             if table[i-1, j] > table[i-1, j-1]:
-                lcss.append(copy.deepcopy(lcss[index]))
+                lcssAppend(index)
                 recon(i-1, j, len(lcss) - 1)
             if table[i, j-1] > table[i-1, j-1]:
-                lcss.append(copy.deepcopy(lcss[index]))
+                lcssAppend(index)
                 recon(i, j-1, len(lcss) - 1)
-            # lcss[index].insert(0, (ocr[i-1],i-1, j-1))
-            lcss[index].insert(0, (i-1, j-1))
+            lcss[index][0] = lcss[index][0] * getProb(ocr[i-1], orig[j-1])
+            # lcss[index][1].insert(0, (ocr[i-1], orig[j-1], i-1, j-1))
+            lcss[index][1].insert(0, (i-1, j-1))
             recon(i-1, j-1, index)
         elif table[i-1, j] == table[i, j-1]:
-            lcss.append(copy.deepcopy(lcss[index]))
+            lcssAppend(index)
             newIndex = len(lcss) - 1
             recon(i-1, j, index)
             recon(i, j-1, newIndex)
@@ -79,5 +102,8 @@ def lcs(ocr, orig):
 
 # print lcs('ABCBDAB', 'BDCABA')
 # print lcs('AGCGA', 'CAGATAGAG')
-# getCEM('../features/characterErrorModelProbalistic.json')
+getCEM('../features/characterErrorModelProbalistic.json')
+minProb = getMinimumProbability()
+increaseProbability(minProb)
 # print lcs('চাহুর','রাহুর')
+print lcs('চাহাচ','হচাচুচাচা')
