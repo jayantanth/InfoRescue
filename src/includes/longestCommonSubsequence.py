fe1fbe730d837654dbb8e4a512088d5f7995fab3
Abhishek_Gupta <knoxxs@hotmail.com>
2013-06-28 01:19:25 +0530

    add common functions to utilities.py

--- src/includes/longestCommonSubsequence.py
+++ src/includes/longestCommonSubsequence.py
@@ -2,48 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import copy
-import json
-import codecs
+import sys
 
-THRESHOLD = 0.4
-modelDict = {}
-minProb = 1
-
-def getCEM(CEMFile):
-    global modelDict
-    with codecs.open(CEMFile, 'r', encoding='utf8') as output:
-        modelDict = json.load(output)
-    for char in modelDict:
-        for mappedChar in modelDict[char]:
-           modelDict[char][mappedChar] = float(modelDict[char][mappedChar])
-
-def getMinimumProbability():
-    return min(min(mappedChars.values()) for mappedChars in modelDict.values())
-
-def increaseProbability(minProb):
-    for origChar in modelDict:
-        for ocrChar in modelDict[origChar]:
-            modelDict[origChar][ocrChar] += minProb
-
-def prob_equals(ocrChar, origChar):
-    try:
-        if modelDict[origChar][ocrChar] >= THRESHOLD:
-            return True
-        return False
-    except KeyError:
-        return False
-
-def getProb(ocrChar, origChar):
-    try:
-        return modelDict[origChar][ocrChar]
-    except KeyError:
-        return minProb
-
-def printTable(table):
-    for row in table:
-        for column in row:
-            print "%3s"%column,
-        print
+sys.path.insert(0, './')
+from utilities import *
 
 def removeDuplicate(lcss):
     setLcss = []
